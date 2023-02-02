5f0c2c688e944086e23fd64538da63e047ded61d
thobela.metu@gmail.com <thobela.metu@gmail.com>
2023-02-02 10:22:01 +0200

    application testing

--- src/components/applicaton/application.test.tsx
+++ src/components/applicaton/application.test.tsx
@@ -4,7 +4,21 @@ import { render, screen } from "@testing-library/react";
 describe('Application', () => { 
     test('renders correctly', () => { 
         render(<Application/>)
+        // checking for the text input
         const nameElement = screen.getByRole('textbox')
         expect(nameElement).toBeInTheDocument()
+
+        // checking by the select element
+        const jobLocationElement = screen.getByRole('combobox')
+        expect(jobLocationElement).toBeInTheDocument()
+
+        // checking by the checkbox element
+        const termsElement = screen.getByRole('checkbox')
+        expect(termsElement).toBeInTheDocument()
+
+        // checking by the submit element
+        const submitButtonElement = screen.getByRole('button')
+        expect(submitButtonElement).toBeInTheDocument()
      })
+
  })
