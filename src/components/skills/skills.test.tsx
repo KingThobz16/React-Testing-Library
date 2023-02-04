129793a18ecbc66b1494704f04df00f8ddb772e0
thobela.metu@gmail.com <thobela.metu@gmail.com>
2023-02-04 18:34:13 +0200

    getAllbyRole

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -3,11 +3,26 @@ import { Skills } from "./skills";
 
 describe('Skills', () => {
     // array of skills we will pass in as props
-    const skills = ["HTML", "CSS", "JavaScript"]
+    const skills = ["HTML", "CSS", "JavaScript", "React"]
 
     // test to check if the component renders correctly
     test('Renders Correctly', () => {
         // we render the component with the props
         render(<Skills skills={skills}/>)
+        // we insure the list is present in the dom
+        const listElement = screen.getByRole('list');
+        // we expect the list on the document
+        expect(listElement).toBeInTheDocument();
     });
+    
+    // lets insure that all the list items
+    //  are also rendered for the given props
+    test('Renders a list of Skills', () => {
+        // we render the component with the props
+        render(<Skills skills={skills}/>)
+        // we then get all the list items using the list item role
+        const listItemElements = screen.getAllByRole('listitem');
+        // we expect the list elements, to have length of skills.length
+        expect(listItemElements).toHaveLength(skills.length);
+    })
 })
