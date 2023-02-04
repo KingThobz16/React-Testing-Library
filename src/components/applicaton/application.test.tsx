4a095ddae6e77f25786bef08cf0e6a04e8f73f0b
Thobela Metu <thobela.metu@gmail.com>
2023-02-04 18:36:40 +0200

    query types

--- src/components/applicaton/application.test.tsx
+++ src/components/applicaton/application.test.tsx
@@ -5,6 +5,11 @@ describe('Application', () => {
     test('renders correctly', () => { 
         render(<Application/>)
 
+// 
+// 
+// getByRole query examples 
+// 
+// 
         // checking for the text input
         const nameElement = screen.getByRole('textbox', {
             // we chose the value of the inputs label as out option
@@ -47,6 +52,51 @@ describe('Application', () => {
             level : 2,
         })
         expect(sectionHeading).toBeInTheDocument()
+
+// 
+// 
+// getByLabelTest query examples 
+// 
+// 
+        // getBylabelText will find the lable element
+        // And then find the input associated with that element.
+        // 
+        const nameElement2 = screen.getByLabelText('Name')
+        // now we expect the input element to be in the document
+        expect(nameElement2).toBeInTheDocument();
+
+        // in the case the are multiple labels with the same Name value
+        // you can use options to be more specific
+        const nameElementOptions = screen.getByLabelText('Name', {
+            selector: "input",
+        })
+        // now we expect the input element to be in the document
+        expect(nameElementOptions).toBeInTheDocument();
+
+        // getBylabelText can also select using the label value
+        // And then find the input associated with that element.
+        const termsElement2 = screen.getByLabelText("I agree with the terms and conditions")
+        expect(termsElement2).toBeInTheDocument();
      })
- 
+
+// 
+// 
+// getByPlaceholdetText query examples 
+// 
+        // Will search for all elements with a placeholder attribute
+        // and find one that matches the given text
+        // const nameElement3 = screen.getByPlaceholderText("bio");
+        // expect(nameElement3).toBeInTheDocument();
+
+
+// 
+// 
+// getByText query examples
+// 
+//      
+        // getByText will search for all elements that have a text node
+        //  with textContent matching the given text
+        const paragraphElement = screen.getByText(/All fields are mandatory/)
+        expect(paragraphElement).toBeInTheDocument();
+
  })
