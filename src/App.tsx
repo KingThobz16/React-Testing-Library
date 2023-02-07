109eae3d435efde59197e9e23d00ab761f991292
Thobela Metu <thobela.metu@gmail.com>
2023-02-07 10:58:04 +0200

    user-event

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import './App.css';
 import Application from './components/applicaton/application';
 import { Skills } from './components/skills/skills';
 import { SkillsProps } from './components/skills/skills.types';
+import { Counter } from './components/counter/counter';
 
 function App() {
   return (
     <div className="App">
-      <Application/>
-      {/* <Skills skills={SkillsProps.skills}/> */}
+      <Counter />
     </div>
   );
 }
